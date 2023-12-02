f4b6eafdf8c634ee4aad07e1121f748134f21920
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-12-02 14:59:46 +0100

    test

--- gpm/helper.py
+++ gpm/helper.py
@@ -21,4 +21,22 @@ def load_gpm_config():
     gpmdata_path = get_gpmdata_path()
     Config = configparser.ConfigParser()
     Config.read(path.join(gpmdata_path, "gpm.config"))
+    # Retrieve values from the configuration file
+    section = config['Section']
+    key1 = section.get('key1')
+    key2 = section.getint('key2')  # Convert to int
+    key3 = section.getboolean('key3')  # Convert to boolean
+    
+    # Parse list from a comma-separated string
+    list_key = section.get('list_key').split(', ')
+    
+    # Return a dictionary with the loaded values
+    config_values = {
+        'key1': key1,
+        'key2': key2,
+        'key3': key3,
+        'list_key': list_key,
+    }
+    
+    return config_values
     return Config
