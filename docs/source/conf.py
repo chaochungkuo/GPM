286f61617fecfef57532815cc6e8191b1f8cf406
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-12-06 21:06:03 +0100

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,4 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath('../..'))
-from gpm.__version__ import version
+from ...gpm.__version__ import version
 
 # Configuration file for the Sphinx documentation builder.
 # -- Project information
