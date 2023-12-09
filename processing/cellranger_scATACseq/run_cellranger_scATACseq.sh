2cf15f63286978a8673766613e9263e90332f37b
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-12-09 21:46:24 +0100

    improve cellranger-atac count

---
+++ processing/cellranger_scATACseq/run_cellranger_scATACseq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# this script will run the scATACseq analysis
+# make sure cellranger is available in your environment
+# otherwise scource shared enviroment from /data/shared_env/shared_paths.sh
+# this script can be run directly from shell
+
+Local_Cores=N_CORES
+refGenome="refdata_cellranger_arc_mm10_2020_A"
+
+mkdir -p ./scATACseq_output
+cd ./scATACseq_output
+
+sampleid="sample"
+fastqDir="FASTQ_PATH"
+PATH_CELLRANGER_ATAC count --sample=$sampleid \
+					--fastqs=$fastqDir \
+					--id=$sampleid \
+					--reference=$refGenome \
+					--localcores=${Local_Cores} \
+					--localmem=120 \
