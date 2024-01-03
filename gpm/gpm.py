73cad1f5085c3a75a774c41a11132ddfdd2b74d0
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2024-01-03 10:57:33 +0100

    fix

--- gpm/gpm.py
+++ gpm/gpm.py
@@ -444,7 +444,7 @@ class GPM():
         # shutil.chown(os.path.join(export_dir, ".htaccess"), group=GROUPNAME)
 
     def create_user(self, export_dir, raw_export=False):
-        export_URL = os.path.join(get_gpm_config("GPM", "EXPORT_URL"),
+        export_URL = os.path.join(get_gpm_config("URL", "EXPORT_URL"),
                                   self.name)
         htpasswd_create_user(export_dir, export_URL, self.provider.lower(),
                              self.app, raw_export)
