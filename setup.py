e8b69d1f7b660782edb21ccaf4861203a9be9923
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-01-03 23:03:34 +0100

    fix

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     url='https://github.com/chaochungkuo/GPM',
     packages=find_packages(),
     install_requires=[
-        'Click', "pandas", "pyyaml", "xtarfile", "tqdm", "gzip"
+        'Click', "pandas", "pyyaml", "xtarfile", "tqdm"
     ],
     entry_points={
         'console_scripts': [
