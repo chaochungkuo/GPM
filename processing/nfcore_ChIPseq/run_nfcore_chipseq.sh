8728e2f6a8e91f25fedbd802d52fabbdf312b2cd
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-01-23 12:12:45 +0100

    update nfcore_chipseq

--- processing/nfcore_ChIPseq/run_nfcore_chipseq.sh
+++ processing/nfcore_ChIPseq/run_nfcore_chipseq.sh
@@ -1,11 +1,11 @@
 ################## test script ##################
 # PATH_NEXTFLOW run nf-core/chipseq -profile test,docker
 
-PATH_NEXTFLOW run nf-core/chipseq -profile docker \
-     --input design.csv \
-     --single_end \
-     --genome gencode_hg38 --macs_gsize 2.7e9 \ # Please define the genome ID: hg38, mm10
-     -name TITLE_NAME
+PATH_NEXTFLOW run nf-core/chipseq -r 2.0.0 -profile docker \
+     --input samplesheet.csv \
+     --single_end --fragment_size 300 \
+     --blacklist /data/genomes/chipseq_blacklists/v3.0/hg38-blacklist.v3.bed \
+     --genome GENCODE_GRCh38_v44 --macs_gsize 2.9e9
 
 # Other optional parameters:
 # --narrow_peak
@@ -29,3 +29,28 @@ PATH_NEXTFLOW run nf-core/chipseq -profile docker \
 
 # Special Cases
 # In some scenarios, H3K4me1, H3K9me2 and H3K9me3 might behave between narrow and broad shape, you might need to look into each peak region and consult experts.
+
+# Genome Effective size
+# GRCh37
+# 2864785220
+# GRCh38
+# 2913022398
+# T2T/CHM13CAT_v2
+# 3117292070
+# GRCm37
+# 2620345972
+# GRCm38
+# 2652783500
+# dm3
+# 162367812
+# dm6
+# 142573017
+# GRCz10
+# 1369631918
+# GRCz11
+# 1368780147
+# WBcel235
+# 100286401
+# TAIR10
+# 119482012
+
