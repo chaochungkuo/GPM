9d5e8a58701af769532ee921106b7f830772369f
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-02-06 10:23:39 +0100

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,30 @@
+import os
+import io
+import re
+#############################################################
+# Get version
+#############################################################
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+current_version = find_version("../../gpm", "__version__.py")
+
 # Configuration file for the Sphinx documentation builder.
 # -- Project information
 
@@ -5,7 +32,7 @@ project = 'GPM'
 copyright = '2024, Chao-Chung Kuo'
 author = 'Chao-Chung Kuo'
 
-release = "0.7.0"
+release = current_version
 
 # -- General configuration
 
