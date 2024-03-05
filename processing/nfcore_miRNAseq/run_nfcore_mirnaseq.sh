60c362e8f746b38b6a4fb61801e501d95a6430fc
Mohamed Mabrouk <mohamed.mabrouk@rwth-aachen.de>
2024-03-05 23:20:59 +0000

    update smrna-seq nf-core pipeline to v2.3.0

--- processing/nfcore_miRNAseq/run_nfcore_mirnaseq.sh
+++ processing/nfcore_miRNAseq/run_nfcore_mirnaseq.sh
@@ -1,31 +1,51 @@
 ################## test script ##################
 # PATH_NEXTFLOW run nf-core/rnaseq -profile test,docker
 
-# mkdir -p PROJECT_FASTQ_PATH/UMI_trimmed
+
+######################################################## Indpendent UMI extraction ########################################################
 # for FASTQ in PROJECT_FASTQ_PATH/*.fastq.gz
-# do
-#     filename=$(basename -- "$FASTQ")
-#     echo $filename
-#     TRIMMED_FASTQ="PROJECT_FASTQ_PATH/UMI_trimmed/${filename}"
-#     echo $TRIMMED_FASTQ
-#     umi_tools extract --stdin=${FASTQ} --stdout=${TRIMMED_FASTQ} --extract-method=regex --bc-pattern='.+AACTGTAGGCACCATCAAT{s<=2}(?P<umi_1>.{12})(?P<discard_2>.*)' 
-# done
+# mkdir -p PROJECT_FASTQ_PATH/UMI_trimmed
+# ls PROJECT_FASTQ_PATH/*.fastq.gz | parallel -j 40 'filename=$(basename -- {});\
+# echo $filename; TRIMMED_FASTQ="PROJECT_FASTQ_PATH/UMI_trimmed/${filename}"; \
+# echo $TRIMMED_FASTQ; umi_tools extract --stdin={} --stdout=${TRIMMED_FASTQ} \
+# --extract-method=regex --bc-pattern=".+AACTGTAGGCACCATCAAT{s<=2}(?P<umi_1>.{12})(?P<discard_2>.*)"'
+
+
 
 ################## GPM samplesheet #####################
 # gpm samplesheet -st 'forward' samplesheet.csv PROJECT_FASTQ_PATH/UMI_trimmed
 
-PATH_NEXTFLOW run nf-core/smrnaseq -r 2.3.0 -profile docker \
-     --input samplesheet.csv --outdir results --mirtrace_species hsa --mirtrace_protocol qiaseq \
+######################## nf-core pipeline #####################################
+# Run the following in the PROJECT_FASTQ_PATH
+# nextflow run nf-core/smrnaseq -r 2.3.0 \
+#      -profile docker \
+#      --input samplesheet.csv \
+#      --genome 'GRCh38' \
+#      --mirtrace_species 'hsa' \
+#      --protocol 'qiaseq' \
+#      --outdir results \
+#      --save_reference \
+
+
+######################################################## Combined Pipeline #################################################################
+
+################## GPM samplesheet ############################
+# gpm samplesheet -st 'forward' samplesheet.csv PROJECT_FASTQ_PATH
+
+
+############### nf-core pipeline ############################### 
+PATH_NEXTFLOW run nf-core/smrnaseq -r 2.3.0 \
+     -profile docker \
+     --input samplesheet.csv \
+     --genome 'GRCh38' \
+     --mirtrace_species 'hsa' \
+     --protocol 'qiaseq' \
+     --outdir results \
+     --save_reference \
      --with_umi \
      --umitools_extract_method regex \
-     --umitools_bc_pattern '.+(?P<discard_1>AACTGTAGGCACCATCAAT){s<=2}(?P<umi_1>.{12})(?P<discard_2>.*)' \
-     --three_prime_adapter AACTGTAGGCACCATCAAT \
-     --protocol qiaseq \
-     --genome GRCh38 \
-     --mirna_gtf /data/genomes/hg38/miRNA/hsa.gff3 \
-     --mature /data/genomes/spikein/QIASeq_miRNAseq_SpikeIn/mature_with_qiaseq_spikein.fa \
-     --hairpin /data/genomes/spikein/QIASeq_miRNAseq_SpikeIn/hairpin_with_qiaseq_spikein.fa
-     
+     --umitools_bc_pattern '.+(?P<discard_1>AACTGTAGGCACCATCAAT){s<=2}(?P<umi_1>.{12})(?P<discard_2>.*)' # Regex pattern for Qiagen_QIAseq_miRNA
+
 # Options for --genome:
 # gencode_hg38, gencode_mm10, hg38, mm10
 # --mirna_gtf /data/genomes/hg38/miRNA/hsa.gff3 --mature /data/genomes/hg38/miRNA/mature.fa.gz --hairpin /data/genomes/hg38/miRNA/hairpin.fa.gz
