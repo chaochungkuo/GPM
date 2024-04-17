89885ceb11562fd3dd908ae8f6837690c58638d3
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-04-17 10:32:47 +0200

    update nfcore_rnaseq script

--- processing/nfcore_RNAseq/run_nfcore_rnaseq.sh
+++ processing/nfcore_RNAseq/run_nfcore_rnaseq.sh
@@ -1,13 +1,31 @@
-################## test script ##################
-# PATH_NEXTFLOW run nf-core/rnaseq -r 3.12.0 -profile test,docker --outdir results
+#!/bin/bash
 
-################## GPM samplesheet #####################
-# gpm samplesheet -st 'reverse' -sn true -si 2 samplesheet.csv FASTQ_PATH
+nfcore_version=3.12.0
 
-PATH_NEXTFLOW run nf-core/rnaseq -r 3.12.0 -profile docker -c nextflow.config \
-     --input samplesheet.csv --outdir results \
-     --genome GENCODE_GRCh38_v44  \
-     --max_cpus 45 --gencode --featurecounts_group_type gene_type
+# Check if the --test option is provided
+if [ "$#" -gt 0 ] && [ "$1" = "--test" ]; then
+  ################## test script #########################
+  PATH_NEXTFLOW run nf-core/rnaseq -r $nfcore_version \
+  -profile test,docker --outdir results
+else
+     ################## GPM samplesheet #####################
+     # gpm samplesheet-rnaseq -st 'reverse' -sn true -si 2 samplesheet.csv PROJECT_FASTQ_PATH
 
-# For rerun the pipeline, use: -resume
-# Options for genome: GENCODE_GRCh38_v44, GENCODE_GRCh38_v44_ERCC
+     ################## Run nfcore pipeline #################
+
+     PATH_NEXTFLOW run nf-core/rnaseq -r 3.12.0 -profile docker -c nextflow.config \
+          --input samplesheet.csv --outdir results \
+          --genome GENCODE_GRCh38_v44  \
+          --gencode --featurecounts_group_type gene_type
+fi
+
+###### For rerun the pipeline #################################
+# use: -resume
+
+###### Options for genome: ####################################
+# GENCODE_GRCh38_v44, GENCODE_GRCh38_v44_ERCC, 
+# GENCODE_GRCm39_v33, GENCODE_GRCm39_v33_ERCC
+# --gencode --featurecounts_group_type gene_type
+
+###### For runs with ERCC spike-ins ############################
+# --genome GENCODE_GRCh38_v44_ERCC --skip_biotype_qc
