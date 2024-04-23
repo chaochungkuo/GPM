67de2948474f49a1d9ca7eba83eb9b2654f18ea6
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2024-04-23 13:43:45 +0200

    add project_ini_struc.py

---
+++ gpm/project_ini_struc.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+
+
+tags_GPM = OrderedDict([("Project", ["date", "name1", "name2", "institute",
+                                     "application", "project.ini",
+                                     "project_path", "project_name",
+                                     "project_string"]),
+                        ("Raw data", ["bcl_path"]),
+                        ("Demultiplexing", ["demultiplex_path",
+                                            "fastq_path",
+                                            "fastq_multiqc_path",
+                                            "demultiplex_method"]),
+                        ("Processing", ["processing_path",
+                                        "processing_method",
+                                        "processing_qc_path",
+                                        "organism",
+                                        "genome_assembly"]),
+                        ("Analysis", ["analysis_path",
+                                      "analysis_types"]),
+                        ("Export", ["export_URL",
+                                    "export_user",
+                                    "export_password"]),
+                        ("History", ["logs"])])
