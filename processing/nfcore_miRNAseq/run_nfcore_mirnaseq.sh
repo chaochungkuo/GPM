dfb63aba9df65f78ed48ebd2831c0246741cfbb2
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-06-23 08:08:11 +0200

    update nfcore/smrnaseq pipeline for spikeins

--- processing/nfcore_miRNAseq/run_nfcore_mirnaseq.sh
+++ processing/nfcore_miRNAseq/run_nfcore_mirnaseq.sh
@@ -2,38 +2,8 @@
 # PATH_NEXTFLOW run nf-core/rnaseq -profile test,docker
 
 
-######################################################## Indpendent UMI extraction ########################################################
-# for FASTQ in PROJECT_FASTQ_PATH/*.fastq.gz
-# mkdir -p PROJECT_FASTQ_PATH/UMI_trimmed
-# ls PROJECT_FASTQ_PATH/*.fastq.gz | parallel -j 40 'filename=$(basename -- {});\
-# echo $filename; TRIMMED_FASTQ="PROJECT_FASTQ_PATH/UMI_trimmed/${filename}"; \
-# echo $TRIMMED_FASTQ; umi_tools extract --stdin={} --stdout=${TRIMMED_FASTQ} \
-# --extract-method=regex --bc-pattern=".+AACTGTAGGCACCATCAAT{s<=2}(?P<umi_1>.{12})(?P<discard_2>.*)"'
+############### nf-core pipeline ###############################
 
-
-
-################## GPM samplesheet #####################
-# gpm samplesheet -st 'forward' samplesheet.csv PROJECT_FASTQ_PATH/UMI_trimmed
-
-######################## nf-core pipeline #####################################
-# Run the following in the PROJECT_FASTQ_PATH
-# nextflow run nf-core/smrnaseq -r 2.3.0 \
-#      -profile docker \
-#      --input samplesheet.csv \
-#      --genome 'GRCh38' \
-#      --mirtrace_species 'hsa' \
-#      --protocol 'qiaseq' \
-#      --outdir results \
-#      --save_reference \
-
-
-######################################################## Combined Pipeline #################################################################
-
-################## GPM samplesheet ############################
-# gpm samplesheet -st 'forward' samplesheet.csv PROJECT_FASTQ_PATH
-
-
-############### nf-core pipeline ############################### 
 PATH_NEXTFLOW run nf-core/smrnaseq -r 2.3.0 \
      -profile docker \
      --input samplesheet.csv \
@@ -48,4 +18,8 @@ PATH_NEXTFLOW run nf-core/smrnaseq -r 2.3.0 \
 
 # Options for --genome:
 # gencode_hg38, gencode_mm10, hg38, mm10
-# --mirna_gtf /data/genomes/hg38/miRNA/hsa.gff3 --mature /data/genomes/hg38/miRNA/mature.fa.gz --hairpin /data/genomes/hg38/miRNA/hairpin.fa.gz
+
+# If the run with QIAseq miRNA Library QC Spike- In Sequences, please add the following options:
+# --mirna_gtf /data/genomes/spikein/QIASeq_miRNAseq_SpikeIn/hsa.gff3 \
+# --mature /data/genomes/spikein/QIASeq_miRNAseq_SpikeIn/mature_with_qiaseq_spikein.fa \
+# --hairpin /data/genomes/spikein/QIASeq_miRNAseq_SpikeIn/hairpin_with_qiaseq_spikein.fa \
