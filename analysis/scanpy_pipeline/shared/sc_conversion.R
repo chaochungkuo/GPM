407f3c733db0ecec8dca96dc67e0926a8d8bfa1d
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2024-07-02 12:17:47 +0200

    addition of output name

--- analysis/scanpy_pipeline/shared/sc_conversion.R
+++ analysis/scanpy_pipeline/shared/sc_conversion.R
@@ -22,8 +22,12 @@ suppressMessages(library(zellkonverter, quietly = T, verbose = F, warn.conflicts
 suppressMessages(library(optparse, quietly = T, verbose = F, warn.conflicts = F))
 
 
-convert_h5ad_to_rds <- function(h5ad_file) {
+convert_h5ad_to_rds <- function(h5ad_file, output = NULL) {
+  if (is.null(output)) {
   sce_file <- sub("\\.h5ad$", ".rds", h5ad_file)
+  } else {
+    sce_file <- output
+  }
   cat(sprintf("Converting %s to %s...\n", h5ad_file, sce_file))
   
   sce <- readH5AD(h5ad_file)
@@ -32,13 +36,16 @@ convert_h5ad_to_rds <- function(h5ad_file) {
   cat(sprintf("Conversion complete: %s\n", sce_file))
 }
 
-convert_rds_to_h5ad <- function(rds_file) {
-  h5ad_file <- sub("\\.rds$", ".h5ad", rds_file)
+convert_rds_to_h5ad <- function(rds_file, output = NULL) {
+
+  if (is.null(output)) {
+    h5ad_file <- sub("\\.rds$", ".h5ad", rds_file)
+  } else {
+    h5ad_file <- output
+  }
   cat(sprintf("Converting %s to %s...\n", rds_file, h5ad_file))
-  
   sce <- readRDS(rds_file)
   writeH5AD(sce, h5ad_file)
-  
   cat(sprintf("Conversion complete: %s\n", h5ad_file))
 }
 
@@ -46,7 +53,9 @@ option_list <- list(
   make_option(c("-i", "--input"), type = "character", default = NULL,
               help = "Input file (.h5ad or .rds)", metavar = "FILE"),
   make_option(c("-o", "--output"), type = "character", default = NULL,
-              help = "Output file (.h5ad or .rds)", metavar = "FILE"
+              help = "Output file (.h5ad or .rds)", metavar = "FILE"),
+  make_option(c("-h", "--help"), action = "store_true", default = FALSE),
+  
 
 )
 
@@ -59,11 +68,12 @@ if (is.null(opt$input)) {
 }
 
 input_file <- opt$input
+output_file <- opt$output
 
 if (grepl("\\.h5ad$", input_file)) {
-  convert_h5ad_to_rds(input_file)
+  convert_h5ad_to_rds(input_file, output_file)
 } else if (grepl("\\.rds$", input_file)) {
-  convert_rds_to_h5ad(input_file)
+  convert_rds_to_h5ad(input_file, output_file)
 } else {
   stop("Unsupported file type. Please provide a .h5ad or .rds file.", call. = FALSE)
 }
