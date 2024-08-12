30c145d5a9caacd8e1804bdec6909fadd0d06168
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2024-08-12 14:00:50 +0200

    complete setup_env.R

--- analysis/scanpy_pipeline/cell-cell-communication/setup_env.R
+++ analysis/scanpy_pipeline/cell-cell-communication/setup_env.R
@@ -1,51 +1,74 @@
-
+package_list = installed.packages()
 
 # Function to process each package
-process_package <- function(package, url) {
-  if (!file.exists(package)) {
-    cat(package, "not found. Downloading...\n")
+process_package <- function(package, package_file, url) {
+    # Check if package is already installed
+    if (package %in% rownames(package_list)) {
+        cat(package, "is already installed\n")
+        return(TRUE)
+    }
+  if (!file.exists(package_file)) {
+    cat(package_file, "not found. Downloading...\n")
     tryCatch({
-      download.file(url, package, mode = "wb")
+      download.file(url, package_file, mode = "wb")
     }, error = function(e) {
-      cat("Failed to download", package, "\n")
+      cat("Failed to download", package_file, "\n")
       return(FALSE)
     })
   }
   
-  cat("Installing", package, "...\n")
-  install.packages(package, repos = NULL, type = "source")
+  cat("Installing", package_file, "...\n")
+  install.packages(package_file, repos = NULL, type = "source")
   
-  if (!file.exists(package)) {
-    cat("Removing downloaded", package, "\n")
-    file.remove(package)
+  if (!file.exists(package_file)) {
+    cat("Removing downloaded", package_file, "\n")
+    file.remove(package_file)
   }
   
   return(TRUE)
 }
 
-# List of packages and their URLs
 packages <- c(
+  "GenomeInfoDbData",
+  "GO.db",
+  "HDO.db",
+  "org.Hs.eg.db",
+  "org.Mm.eg.db"
+)
+
+# List of packages and their URLs
+packages_files <- c(
   "GenomeInfoDbData_1.2.10.tar.gz",
   "GO.db_3.17.0.tar.gz",
   "HDO.db_0.99.1.tar.gz",
   "org.Hs.eg.db_3.17.0.tar.gz",
   "org.Mm.eg.db_3.17.0.tar.gz"
-  
 )
 
 # Read URLs from download.txt
-urls <- readLines("download.txt")
-
-if (length(packages) != length(urls)) {
-  stop("Number of packages does not match number of URLs in download.txt")
-}
+urls <- c("https://bioconductor.org/packages/3.17/data/annotation/src/contrib/GenomeInfoDbData_1.2.10.tar.gz",
+        "https://bioconductor.org/packages/3.17/data/annotation/src/contrib/GO.db_3.17.0.tar.gz",
+        "https://bioconductor.org/packages/3.17/data/annotation/src/contrib/HDO.db_0.99.1.tar.gz",
+        "https://bioconductor.org/packages/3.17/data/annotation/src/contrib/org.Mm.eg.db_3.17.0.tar.gz",
+        "https://bioconductor.org/packages/3.17/data/annotation/src/contrib/org.Hs.eg.db_3.17.0.tar.gz"
+        )
 
 # Process each package
 for (i in seq_along(packages)) {
-  process_package(packages[i], urls[i])
+  process_package(packages[i], packages_files[i],  urls[i])
 }
 
-install.packages("netdiffuseR", repos='http://cran.us.r-project.org')
+if ("netdiffuseR" %in% rownames(package_list)) {
+    cat("netdiffuseR", "is already installed\n")
+}else {
+   install.packages("netdiffuseR", repos='http://cran.us.r-project.org')
 
-system("wget https://drive.google.com/file/d/12i65hJl3LMeum2W4dWbyIZJudz5310Xu/view?usp=sharing")
+}
+
+if ("CrossTalkeR" %in% rownames(package_list)) {
+    cat("CrossTalkeR", "is already installed\n")
+}else {
+system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=115M510CCnjzwXVGd3_B3RdcntvP0UPyw' -O CrossTalkeR.tar.gz")
 system("R CMD INSTALL CrossTalkeR.tar.xz")
+file.remove("CrossTalkeR.tar.gz")
+}
