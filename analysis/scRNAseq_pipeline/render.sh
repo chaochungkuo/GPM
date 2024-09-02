dd5ae692e855c4e54c18836f7d62a1454f980b94
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2024-09-02 19:55:47 +0200

    add global pixi env

--- analysis/scRNAseq_pipeline/render.sh
+++ analysis/scRNAseq_pipeline/render.sh
@@ -1,9 +1,4 @@
-quarto render preprocessing/1_quality_control.ipynb --execute -to html
-jupyter nbconvert --clear-output --inplace preprocessing/1_quality_control.ipynb
-
-quarto render preprocessing/2_normalization.ipynb --execute -to html
-jupyter nbconvert --clear-output --inplace preprocessing/2_normalization.ipynb
-
-quarto render annotation/3_annotation.ipynb --execute -to html
-jupyter nbconvert --clear-output --inplace annotation/3_annotation.ipynb
+quarto render preprocessing/1_quality_control.ipynb --execute --to html && jupyter nbconvert --clear-output --inplace preprocessing/1_quality_control.ipynb
+quarto render preprocessing/2_normalization.ipynb --execute --to html && jupyter nbconvert --clear-output --inplace preprocessing/2_normalization.ipynb
+quarto render annotation/3_annotation.ipynb --execute --to html && jupyter nbconvert --clear-output --inplace annotation/3_annotation.ipynb
 
