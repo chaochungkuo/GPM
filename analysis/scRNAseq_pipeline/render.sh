eaf5b26002c808880d6738c7a1b788f9f8c9ce04
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2024-09-02 16:12:40 +0200

    clear output after redndering in render.sh

--- analysis/scRNAseq_pipeline/render.sh
+++ analysis/scRNAseq_pipeline/render.sh
@@ -1,3 +1,9 @@
 quarto render preprocessing/1_quality_control.ipynb --execute -to html
+jupyter nbconvert --clear-output --inplace preprocessing/1_quality_control.ipynb
+
 quarto render preprocessing/2_normalization.ipynb --execute -to html
-quarto render preprocessing/3_annotation.ipynb --execute -to html
+jupyter nbconvert --clear-output --inplace preprocessing/2_normalization.ipynb
+
+quarto render annotation/3_annotation.ipynb --execute -to html
+jupyter nbconvert --clear-output --inplace annotation/3_annotation.ipynb
+
