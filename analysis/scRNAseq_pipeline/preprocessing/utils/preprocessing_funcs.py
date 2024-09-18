13259218deb6c1faf1d1ae617eb1b097dd56bcad
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2024-09-18 17:12:13 +0200

    initial work on refactoring outlier detection

--- analysis/scRNAseq_pipeline/preprocessing/utils/preprocessing_funcs.py
+++ analysis/scRNAseq_pipeline/preprocessing/utils/preprocessing_funcs.py
@@ -1,6 +1,5 @@
-from functools import reduce
 from numbers import Number
-from os import listdir, mkdir, path, walk
+from os import path
 from typing import Callable, Dict, List
 
 import numpy as np
@@ -8,6 +7,7 @@ import pandas as pd
 import requests
 import scanpy as sc
 from anndata import AnnData
+from scipy.sparse import issparse
 from scipy.stats import median_abs_deviation
 
 ###------------------------------------------------------------------------------------------------------------------------------------------------------------###
@@ -107,7 +107,7 @@ inputs: Dict[str, List | Callable] = {
 }
 
 
-qc_features_fac: Dict[str, List[str]] = {
+qc_features_rules: Dict[str, List[str]] = {
     "human": {"mito": ["MT-"], "ribo": ["RBS", "RPL"], "hb": ["^HB[^(P)]"]},
     "mouse": {
         "mito": ["mt"],
@@ -137,83 +137,9 @@ def human2mouse(genes: List[str]) -> List[str]:
     return df.name.replace("N/A", pd.NA).dropna().to_list()
 
 
-from functools import reduce
-from numbers import Number
-from os import listdir, mkdir, path, walk
-from typing import Callable, Dict, List
-
-import numpy as np
-import pandas as pd
-import requests
-import scanpy as sc
-from anndata import AnnData
-from scipy.stats import median_abs_deviation
-
 ###------------------------------------------------------------------------------------------------------------------------------------------------------------###
 ###                                                            Reading Functions                                                                               ###
 ###------------------------------------------------------------------------------------------------------------------------------------------------------------###
-
-
-# Configs
-## Utility Functions
-def get_sample_name(file_path: str, black_list: list[str], n=3):
-    """ "Function to return probable sample name from a path, it recurselvy goes through the path and returns the first element not in the black list."""
-    if n == 0:
-        return ""
-
-    tmp = path.basename(file_path)
-    _d = path.dirname(file_path)
-
-    if all(entry not in tmp for entry in black_list):
-        return tmp
-    else:
-        res = get_sample_name(_d, black_list, n - 1)
-    return res
-
-
-def is_outlier(adata: AnnData, metric: str, nmads: int):
-    M = adata.obs[metric]
-    outlier = (M < np.median(M) - nmads * median_abs_deviation(M)) | (
-        np.median(M) + nmads * median_abs_deviation(M) < M
-    )
-    return outlier
-
-
-def read_parsebio(data_path: str) -> AnnData:
-    """Reads ParseBio
-
-    Args:
-        data_path (str): path to the parsebio output.
-
-    Returns:
-        AnnData: Return AnnData object containing .X, .obs, and .var components
-    """
-
-    adata = sc.read_mtx(data_path + "count_matrix.mtx")
-
-    # reading in gene and cell data
-    gene_data = pd.read_csv(data_path + "all_genes.csv")
-    cell_meta = pd.read_csv(data_path + "cell_metadata.csv")
-
-    # find genes with nan values and filter
-    gene_data = gene_data[gene_data.gene_name.notnull()]
-    notNa = gene_data.index
-    notNa = notNa.to_list()
-
-    # remove genes with nan values and assign gene names
-    adata = adata[:, notNa]
-    adata.var = gene_data
-    adata.var.set_index("gene_name", inplace=True)
-    adata.var.index.name = None
-    adata.var_names_make_unique()
-
-    # add cell meta data to anndata object
-    adata.obs = cell_meta
-    adata.obs.set_index("bc_wells", inplace=True)
-    adata.obs.index.name = None
-    adata.obs_names_make_unique()
-
-    return adata
 
 
 ## Technology components
@@ -245,7 +171,7 @@ inputs: Dict[str, List | Callable] = {
 }
 
 
-qc_features_fac: Dict[str, List[str]] = {
+qc_features_rules: Dict[str, List[str]] = {
     "human": {"mito": ["MT-"], "ribo": ["RBS", "RPL"], "hb": ["^HB[^(P)]"]},
     "mouse": {
         "mito": ["mt"],
@@ -255,86 +181,110 @@ qc_features_fac: Dict[str, List[str]] = {
 }
 
 
-###------------------------------------------------------------------------------------------------------------------------------------------------------------###
+###------------------------------------------------------------------------------------------------------------------------------------###
 ###                                                            QC Functions                                                                                    ###
 ###------------------------------------------------------------------------------------------------------------------------------------------------------------###
 
 
-def human2mouse(genes: List[str]) -> List[str]:
-    r = requests.post(
-        url="https://biit.cs.ut.ee/gprofiler/api/orth/orth/",
-        json={
-            "organism": "hsapiens",
-            "target": "mmusculus",
-            "query": genes,
-        },
-    )
-    df = pd.DataFrame(
-        r.json()["result"],
-    )
-    return df.name.replace("N/A", pd.NA).dropna().to_list()
+def _compute_outliers(
+    df: pd.DataFrame,
+    variable: str,
+    value: List | Number,
+    max_only: bool = False,
+    log_transform: bool = False,
+) -> pd.DataFrame:
+    """computes outliers for the given variable in the dataframe
 
+    Args:
+        adata (AnnData): Input AnnData object
+        variable: name of the column to use to for outlier detection
+        value: value to use for outlier detection, if a list is provided, it is used as the lower and upper bound
+    Returns:
+        df: the input dataframe with an additional column for the outliers
+    """
 
-def reduce_outliers(
-    adata: AnnData,
-    variables: Dict[str, List | Number] | Dict[str, Dict[str, List | Number]],
-    subset: bool = False,
-) -> pd.Series:
-    if subset:
-        adata.obs["outlier"] = False
-        for sample in variables.keys():
-            _compute_outlier_sample(adata, variables, sample)
+    if isinstance(value, list):
+
+        min_val = value[0]
+        max_val = value[1]
+        max_only = False  # Make sure not to override the user input
+
+    if isinstance(value, Number):
+        if not value > 0:
+            raise ValueError("Please provide a positive number of nmads")
+
+        if log_transform:
+            series = np.log1p(df[variable])
+        else:
+            series = df[variable]
+
+        min_val = np.median(series) - (median_abs_deviation(series) * value)
+        max_val = np.median(series) + (median_abs_deviation(series) * value)
+
+    if max_only:
+        df[f"{variable}_outlier"] = df[variable].gt(max_val)
     else:
-        _compute_outlier_all(adata, variables)
+        df[f"{variable}_outlier"] = df[variable].lt(min_val) | df[variable].gt(max_val)
+
+    df[f"{variable}_outlier"].fillna(False)
+
+    return df
 
 
-def _compute_outlier_all(adata: AnnData, variables: Dict[str, List]) -> pd.Series:
-    for key in variables.keys():
-        if not key in adata.obs.columns:
-            raise KeyError(
-                "the provided QC variable does not exist in the data, check the variable names again."
-            )
-
-        if isinstance(variables[key], list):
-            if len(variables[key]) != 2:
-                raise ValueError(
-                    "Provide a list of length 2 for the lower and upper bound of the QC-variable."
-                )
-            min_val = variables[key][0]
-            max_val = variables[key][1]
-
-        if isinstance(variables[key], Number):
-            if not variables[key] > 0:
-                raise ValueError("Please provide a positive number of nmads")
-
-            min_val = np.median(adata.obs[key]) - (
-                median_abs_deviation(adata.obs[key]) * variables[key]
-            )
-            max_val = np.median(adata.obs[key]) + (
-                median_abs_deviation(adata.obs[key]) * variables[key]
-            )
-
-        adata.obs[f"{key}_outlier"] = adata.obs[key].lt(min_val) | adata.obs[key].gt(
-            max_val
+def compute_outliers_var_bulk(
+    df: pd.DataFrame,
+    variable: str,
+    value: List | Number,
+    max_only: bool = False,
+    log_transform: bool = False,
+):
+    
+    # Validate the input
+    if variable not in df:
+        raise KeyError(
+            "the provided QC variable does not exist in the data, check the variable names again."
         )
-        adata.obs[f"{key}_outlier"].fillna(False)
 
-    adata.obs["outlier"] = adata.obs[[f"{x}_outlier" for x in variables.keys()]].any(
-        axis=1
-    )
+    if not isinstance(value, (list, Number)):
+        raise ValueError(
+            "Please provide a positive number of nmads or a list of length 2 for the lower and upper bound of the QC-variable."
+        )
+
+    if isinstance(value, Number):
+        if not value > 0:
+            raise ValueError("Please provide a positive number of nmads")
 
 
-def _compute_outlier_sample(adata: AnnData, variables: Dict[str, List], sample):
+    return _compute_outliers(df, variable, value, max_only, log_transform) 
+
+
+
+
+
+def compute_outlier_sample(
+    adata: AnnData,
+    variable: str,
+    value: List | Number,
+    sample: str,
+    max_only: bool = False,
+    log_transform: bool = False,
+) -> None:
+    adata.obs["outlier"] = False
+    for sample in variables.keys():
+        _compute_outlier_sample(adata, variables, sample)
+
+
+def _compute_outlier_sample(adata: AnnData, variables: Dict[str, List], sample) -> None:
     sample_dict = variables[sample]
     sample_slice = adata.obs.query(f"sample == '{sample}'")
 
     for key in sample_dict.keys():
-        if not key in sample_slice.columns:
+        if key not in sample_slice.columns:
             raise KeyError(
                 "the provided QC variable does not exist in the data, check the variable names again."
             )
 
-        if not f"{key}_outlier" in sample_slice.columns:
+        if f"{key}_outlier" not in sample_slice.columns:
             sample_slice[f"{key}_outlier"] = False
 
         if isinstance(sample_dict[key], list):
@@ -398,33 +348,6 @@ def get_var_features_num(adata: AnnData, variable_features: int | float) -> int:
 
 
 def is_raw_counts(matrix) -> bool:
-    from scipy.sparse import issparse
-
-    if issparse(matrix):
-        return matrix.count_nonzero() == matrix.astype("uint32").count_nonzero()
-    else:
-        return np.count_nonzero(matrix) == np.count_nonzero(matrix.astype("uint32"))
-
-
-def get_keys(qc_dict):
-    keys_list = []
-    if len(qc_dict) > 0 and all(
-        map(lambda x: isinstance(x, (list, Number)), qc_dict.values())
-    ):
-        return list(qc_dict.keys())
-
-    if len(qc_dict) > 0 and all(
-        map(lambda x: isinstance(x, (dict, Number)), qc_dict.values())
-    ):
-        for key in qc_dict.keys():
-            keys_list = keys_list + list(qc_dict[key].keys())
-        return list(set(keys_list))
-
-    return []
-
-
-def is_raw_counts(matrix) -> bool:
-    from scipy.sparse import issparse
 
     if issparse(matrix):
         return matrix.count_nonzero() == matrix.astype("uint32").count_nonzero()
--- analysis/scRNAseq_pipeline/readme.md
+++ analysis/scRNAseq_pipeline/readme.md
@@ -3,3 +3,13 @@
 - [ ]  Finish the clustering pipeline.
 - [ ]  Finish the Annotation pipeline.
 - [ ]  Start Differential expression module.
+- [ ]  
+
+
+TODO - QC:
+- [ ] Make QC for all samples invidually and then merge them.
+- [ ] Refactor the sample discovery code
+- [ ] Add Documentation for the usage of the QC module.
+- [ ] Simpify the plotting of the QC module and clean up the manifest file.
+- [ ] Turn on doublet detection and Ambient RNA detection.
+ 
