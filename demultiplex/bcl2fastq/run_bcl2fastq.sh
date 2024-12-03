4049cbe8d6b67e37d2936fa635609225ef998de0
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2024-12-03 09:05:58 +0100

    add controler for running fastq_screen

--- demultiplex/bcl2fastq/run_bcl2fastq.sh
+++ demultiplex/bcl2fastq/run_bcl2fastq.sh
@@ -3,6 +3,9 @@
 # screen -S bcl2fastq
 # bash run_bcl2fastq.sh
 
+# Define the boolean variable for running fastq_screen
+run_fastq_screen=true  # Set to true or false as needed
+
 # Get the nummber of jobs as ncors - 10 or 1
 NJOBS=$(( $(nproc) - 10 > 0 ? $(nproc) - 10 : 1 ))
 
@@ -19,13 +22,21 @@ PATH_BCL2FASTQ \
 mkdir -p ./fastqc
 find * -maxdepth 1 -name "*.fastq.gz" | parallel -j $NJOBS "fastqc {} -o ./fastqc"
 
-###### Running fastq_screen ######################################
-mkdir -p ./fastq_screen
-find * -maxdepth 1 -name "*.fastq.gz" | parallel -j $NJOBS "fastq_screen --outdir ./fastq_screen {}"
+###### Running fastq_screen (Conditional Execution) ########
+if [ "$run_fastq_screen" = true ]; then
+  echo "Running fastq_screen..."
+  mkdir -p ./fastq_screen
+  find * -maxdepth 1 -name "*.fastq.gz" | parallel -j $NJOBS "fastq_screen --outdir ./fastq_screen {}"
+else
+  echo "Skipping fastq_screen as run_fastq_screen is set to false."
+fi
 
 ###### Running MultiQC #####################################
 mkdir -p multiqc
-multiqc -f ./fastq_screen/ . -o ./multiqc
+multiqc -f . -o ./multiqc
 
-rm -r ./fastqc ./fastq_screen
-
+# Cleanup
+rm -r ./fastqc
+if [ -d "./fastq_screen" ]; then
+  rm -r ./fastq_screen
+fi
