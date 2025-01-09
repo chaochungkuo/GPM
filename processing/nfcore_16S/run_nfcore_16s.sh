849ef94799d43f7263a6954fe0ab953ddbc1c8eb
lganRWTH <lgan@ukaachen.de>
2025-01-09 12:06:08 +0100

    modify 16s nfcore and add report

--- processing/nfcore_16S/run_nfcore_16s.sh
+++ processing/nfcore_16S/run_nfcore_16s.sh
@@ -1,13 +1,16 @@
 ################## test script ##################
 # PATH_NEXTFLOW run nf-core/ampliseq -profile test,docker --outdir ./results
+# switch to 16s enviroment before run this pipe
 
 PATH_NEXTFLOW run nf-core/ampliseq  -profile docker \
     --input samplesheet.csv \
     --metadata metadata.tsv \
     --FW_primer "YYYRYGRDDBVCWSCA" \
     --RV_primer "GAHTACNVRRGTNTCTAAKYY" \
+    --min_frequency 10 \
+    --outdir ./results_16s 
+
 #    --FW_primer "CCTACGGGDGGCWGCAG", "CCTAYGGGGYGCWGCAG" \
 #    --RV_primer "GACTACNVGGGTMTCTAATCC" \
 #   --FW_primer "GTGYCAGCMGCCGCGGTAA" \ 
 #   --RV_primer "GGACTACNVGGGTWTCTAAT" \
-    --outdir ./results
