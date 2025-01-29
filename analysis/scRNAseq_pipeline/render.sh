6bd52e05e460239bc1863919286791dabff1cb67
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2025-01-29 16:04:13 +0100

    update scRNAseq preprocessing render.sh and fix normalization error when there is only 1 cluster

--- analysis/scRNAseq_pipeline/render.sh
+++ analysis/scRNAseq_pipeline/render.sh
@@ -5,14 +5,14 @@ EXECUTE_FLAG="--execute"
 
 # Preprocessing
 cd preprocessing
-pixi run quarto render 1_quality_control.ipynb $EXECUTE_FLAG --to html && \
-jupyter nbconvert --clear-output --inplace 1_quality_control.ipynb
-pixi run quarto render 2_normalization.ipynb $EXECUTE_FLAG --to html && \
-jupyter nbconvert --clear-output --inplace 2_normalization.ipynb
+pixi run quarto render 1_quality_control.ipynb $EXECUTE_FLAG --to html
+pixi run jupyter nbconvert --clear-output --inplace 1_quality_control.ipynb
+pixi run quarto render 2_normalization.ipynb $EXECUTE_FLAG --to html
+pixi run jupyter nbconvert --clear-output --inplace 2_normalization.ipynb
 cd ../
 
 # Annotation
 cd annotation
-pixi run quarto render 3_annotation.ipynb $EXECUTE_FLAG --to html && \
-jupyter nbconvert --clear-output --inplace 3_annotation.ipynb
+pixi run quarto render 3_annotation.ipynb $EXECUTE_FLAG --to html
+pixi run jupyter nbconvert --clear-output --inplace 3_annotation.ipynb
 cd ../
